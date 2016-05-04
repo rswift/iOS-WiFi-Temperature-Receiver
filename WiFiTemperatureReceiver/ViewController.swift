64fd42f78fa76a535486d7a4b18e8eac11ae9f1c
Robert Swift <robert.swift@me.com>
2016-05-04 13:23:14 +0100

    Initial Commit

---
+++ WiFiTemperatureReceiver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WiFiTemperatureReceiver
+//
+//  Created by Robert on 04/05/2016.
+//  Copyright © 2016 vooey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
